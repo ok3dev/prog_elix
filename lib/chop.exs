5e928bcd424fdaea1ca079b084ad4cd4d07adbf8
Leo Mendoza Jr <leo@ok3.dev>
2021-01-04 12:00:00 -0500

    refactor to capture range as an argument

--- lib/chop.exs
+++ lib/chop.exs
@@ -1,19 +1,23 @@
 defmodule Chop do
-  def guess(actual, a..b) do
-    guess = div(b + a, 2)
+  def guess(actual, a..b = range) do
+    # In Dave Thomas's code, he used def guess(actual, range = low..high)
+    # Putting `range` on either side will still bind whatever argument is
+    # passed second to `range`
+    guess = div(a + b, 2)
     IO.puts("Is it #{guess}")
-    adjust_guess(actual, guess, a, b)
+    adjust_guess(actual, guess, range)
   end
 
-  def adjust_guess(actual, guess, _a, _b) when actual === guess do
+  defp adjust_guess(actual, guess, _a.._b = _range) when actual === guess do
     "#{actual} is the correct guess!"
   end
 
-  def adjust_guess(actual, guess, a, _b) when actual < guess do
+  defp adjust_guess(actual, guess, a.._b = _range) when actual < guess do
     guess(actual, a..(guess - 1))
   end
 
-  def adjust_guess(actual, guess, _a, b) when actual > guess do
+  defp adjust_guess(actual, guess, _a..b = _range) when actual > guess do
     guess(actual, (guess + 1)..b)
   end
+
 end
