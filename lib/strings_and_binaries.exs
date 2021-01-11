32bd7c736e6a22ed745f1cb7e666e27602b5463f
Leo Mendoza Jr <leo@ok3.dev>
2021-01-11 13:15:41 -0500

    attempt StringsAndBinaries-5

--- lib/strings_and_binaries.exs
+++ lib/strings_and_binaries.exs
@@ -6,4 +6,13 @@ defmodule StringsAndBinaries do
   end
 
   def anagram?(word1, word2), do: word1 -- word2 === []
+
+  # Exercise: StringsAndBinaries-5
+  def center(words) do
+    width = Enum.max_by(words, &String.length(&1)) |> String.length()
+
+    Enum.each(words, fn word ->
+      String.pad_leading(word, div(width - String.length(word), 2)) |> IO.puts()
+    end)
+  end
 end
